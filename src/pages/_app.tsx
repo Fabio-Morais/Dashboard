bad22b537998cc4a5fd75cf379fa74d4609abde0
Fábio <fabiouds@hotmail.com>
2023-05-05 16:44:35 +0100

    fix dark mode bug

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
@@ -11,11 +11,15 @@ import { theme } from '@/styles/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient())
+  const [isReady, setIsReady] = useState(false)
 
+  useEffect(() => {
+    setIsReady(true)
+  }, [])
   return (
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
+        {isReady && <Component {...pageProps} />}
         {process.env.NODE_ENV === 'development' && <ReactQueryDevtools initialIsOpen={false} />}{' '}
       </QueryClientProvider>
     </ChakraProvider>
